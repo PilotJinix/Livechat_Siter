0e3f5871f1ff27465b58cdc56221630b414c0fa4
PilotJinix <agungsantoso557@gmail.com>
2021-04-13 15:53:48 +0700

    make Models

--- backend/app/Models/Comment.ts
+++ backend/app/Models/Comment.ts
@@ -1,25 +1,19 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
-import {NewsInterface} from "@interfaces/model";
+import {CommentsInterface} from "@interfaces/model";
 
-export default class News extends BaseModel implements NewsInterface{
+export default class Comment extends BaseModel implements CommentsInterface{
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public authorId: number;
+  public newsId: number;
 
   @column()
-  public title: string;
+  public userId: number;
 
   @column()
-  public content: string;
-
-  @column()
-  public slug: string;
-
-  @column()
-  public thumbnail: string;
+  public comment: string;
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
